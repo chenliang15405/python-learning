4ef81e462a4f9db2f0acb215ccafca9a861b96f9
chenliang15405 <1165243776@qq.com>
2019-11-11 22:45:26 +0800

    feat: tcp文件下载器

--- study_day04-网络编程/06_文件下载器-server.py
+++ study_day04-网络编程/06_文件下载器-server.py
@@ -2,3 +2,34 @@
 文件下载服务端
 
 """
+import socket
+
+
+def main():
+    # 创建socket
+    tcp_server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # 绑定端口和ip
+    tcp_server_socket.bind(("", 8080))
+
+    # 让socket设置为监听模式
+    # 128表示多个客户端可以同时链接
+    tcp_server_socket.listen(128)
+
+    # 等待客户端链接
+    new_clinet_socket, client_addr = tcp_server_socket.accept()
+
+    # 接收客户端数据
+    file_name = new_clinet_socket.recv(1024)
+    print(file_name)
+
+    # 响应数据给客户端
+    new_clinet_socket.send("-----------ok--------".encode("utf-8"))
+
+    # 关闭socket
+    new_clinet_socket.close()
+    tcp_server_socket.close()
+
+
+if __name__ == '__main__':
+    main()
--- study_day04-网络编程/07_文件下载器-client.py
+++ study_day04-网络编程/07_文件下载器-client.py
@@ -2,3 +2,33 @@
 文件下载客户端
 
 """
+import socket
+
+
+def main():
+    # 创建socket
+    tcp_client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # 链接服务器
+    server_addr = ("", 8080)
+    tcp_client_socket.connect(("127.0.0.1", 8080))
+
+    # 输入下载的文件名
+    download_file_name = input("输入需要下载的文件名")
+
+    # 发送数据到服务器
+    tcp_client_socket.send(download_file_name.encode("utf-8"))
+
+    # 接收返回数据
+    recv_data = tcp_client_socket.recv(1024*1024)
+
+    # 保存数据到文件中
+    with open("[新]" + download_file_name, "wb") as f:
+        f.write(recv_data)
+
+    # 关闭socket
+    tcp_client_socket.close()
+
+
+if __name__ == '__main__':
+    main()
